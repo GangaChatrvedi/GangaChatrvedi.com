bb757f504cae85b456095ac9c869e36399502713
GangaChatrvedi <ganga.chaturvedi24@gmail.com>
2020-03-05 13:06:59 +0530

    hanges

--- src/config/index.js
+++ src/config/index.js
@@ -1,18 +1,17 @@
 module.exports = {
-  siteTitle: 'Brittany Chiang | Software Engineer',
-  siteDescription:
-    'Brittany Chiang is a software engineer based in Boston, MA who specializes in developing (and occasionally designing) exceptional, high-quality websites and applications.',
+  siteTitle: 'Ganga Chaturvedi | Full Stack Developer',
+  siteDescription: 'Ganga Chaturvedi is a full stack developer based in Delhi, India.',
   siteKeywords:
-    'Brittany Chiang, Brittany, Chiang, bchiang7, software engineer, front-end engineer, web developer, javascript, northeastern',
-  siteUrl: 'https://brittanychiang.com',
+    'Ganga Chiang, Brittany, Chiang, bchiang7, software engineer, front-end engineer, web developer, javascript, northeastern',
+  siteUrl: 'https://gangachatrvedi.com',
   siteLanguage: 'en_US',
   googleAnalyticsID: 'UA-45666519-2',
   googleVerification: 'DCl7VAf9tcz6eD9gb67NfkNnJ1PKRNcg8qQiwpbx9Lk',
-  name: 'Brittany Chiang',
-  location: 'Boston, MA',
-  email: 'brittany.chiang@gmail.com',
-  github: 'https://github.com/bchiang7',
-  twitterHandle: '@bchiang7',
+  name: 'Ganga Chaturvedi',
+  location: 'Delhi, India',
+  email: 'ganga.chaturvedi24@gmail.com',
+  github: 'https://github.com/GangaChatrvedi/',
+  twitterHandle: '@GangaChatrvedi',
   socialMedia: [
     {
       name: 'GitHub',
