9dfba8972698e57ba45ed0bd7ac91850443391a1
GangaChatrvedi <ganga.chaturvedi24@gmail.com>
2020-03-22 15:36:08 +0530

    correction in name

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,8 +16,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'BrittanyChiang',
-        short_name: 'BrittanyChiang',
+        name: 'GangaChatrvedi',
+        short_name: 'GangaChatrvedi',
         start_url: '/',
         background_color: config.darkNavyColor,
         theme_color: config.navyColor,
